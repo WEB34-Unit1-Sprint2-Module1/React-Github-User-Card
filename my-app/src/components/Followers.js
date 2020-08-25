ac871315009b83936401b230308690dc7ce768df
alexis villaraza <alexis.borja@gmail.com>
2020-08-25 11:26:53 -0700

    displays followers

--- my-app/src/components/Followers.js
+++ my-app/src/components/Followers.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Followers extends Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return (
       <div>
-        <p>This is followers</p>
+        {this.props.followers.map((follower) => (
+          <div>
+            <h2>{follower.name}</h2>
+            <img
+              src={follower.avatar_url}
+              alt={`Profile of ${follower.name}`}
+            />
+            <p>
+              <a href={follower.html_url}>{follower.login}</a>
+            </p>
+          </div>
+        ))}
       </div>
-    )
+    );
   }
 }
 
